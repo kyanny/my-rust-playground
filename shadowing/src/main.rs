fdcf0e3c47c701c876a5c68ad5264169fdc8f4ac
Kensuke Nagae <kyanny@gmail.com>
2022-04-27 01:29:39 +0900

    shadowing: different type ok

--- shadowing/src/main.rs
+++ shadowing/src/main.rs
@@ -3,4 +3,9 @@ fn main() {
     let x = x + 1;
     let x = x * 2;
     println!("The value of x is :{}", x);
+
+    let spaces = "  ";
+    println!("spaces=[{}]", spaces);
+    let spaces = spaces.len();
+    println!("spaces=[{}]", spaces);
 }
