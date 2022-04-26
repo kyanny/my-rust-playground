8482eda4b5590db943ac0d336ec196c12f63ba71
Kensuke Nagae <kyanny@gmail.com>
2022-04-27 01:25:33 +0900

    const

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,12 @@ fn main() {
     println!("The vaue of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    println!("MAX_POINTS = {}", MAX_POINTS);
 }
+
+// let a = 1;
+
+// const B : i32 = 2;
+
+const MAX_POINTS: u32 = 100_000;
