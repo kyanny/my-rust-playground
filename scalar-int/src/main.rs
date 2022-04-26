40b2716a75d6e530b95ac06dd6123f5d6effac70
Kensuke Nagae <kyanny@gmail.com>
2022-04-27 01:49:36 +0900

    numeric operations

--- scalar-int/src/main.rs
+++ scalar-int/src/main.rs
@@ -2,6 +2,21 @@ fn main() {
     let n : i8 = 127;
     println!("n=[{}]", n);
 
-    let nn = n + 1;
-    println!("nn=[{}]", nn);
+    // let nn = n + 1;
+    // println!("nn=[{}]", nn);
+
+    let sum = 5 + 10;
+    println!("sum: {}", sum);
+
+    let difference = 95.5 - 4.3;
+    println!("difference: {}", difference);
+
+    let product = 4 * 30;
+    println!("product: {}", product);
+
+    let quotient = 56.7 / 32.2;
+    println!("quotient: {}", quotient);
+
+    let remainder = 43 % 5;
+    println!("remainder: {}", remainder);
 }
