450124a9a590c24373c73d17ac3789cb96d00ad5
Kensuke Nagae <kyanny@gmail.com>
2022-05-05 20:23:24 +0900

    impl

--- cgi-remote-addr/src/main.rs
+++ cgi-remote-addr/src/main.rs
@@ -1,6 +1,11 @@
 extern crate cgi;
 
 cgi::cgi_main! { |request: cgi::Request| -> cgi::Response {
-    let world = "world";
-    cgi::text_response(200, format!("Hello {}", world))
+    let headers = request.headers();
+    let mut x: String = "".to_string();
+    for key in headers.keys() {
+        x = x + "\n" + key.as_str();
+    }
+    let remote_addr = request.headers().get("x-cgi-remote-addr").unwrap();
+    cgi::text_response(200, format!("Hello {}, {:?}", x, remote_addr))
 } }
